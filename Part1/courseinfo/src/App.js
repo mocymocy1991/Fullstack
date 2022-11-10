7d1df6fa21dca74fae3049259cc6c7639f96163a
Ziwei Tang <tangziwei1991@gmail.com>
2022-11-10 17:05:01 +0100

    Completed 1.4

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -16,47 +16,47 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
+  let parts = props.parts;
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </div>
   );
 };
 
 const Footer = (props) => {
+  let parts = props.parts;
   return (
     <p>
       Number of exercises{" "}
-      {props.exercises1 + props.exercises2 + props.exercises3}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
     </p>
   );
 };
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Footer
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Footer parts={parts} />
     </div>
   );
 };
